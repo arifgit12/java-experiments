be46446336a410738bb2f2f2f911605dd145e574
Arif A Mondal <arifali.mondal@gmail.com>
2023-12-05 23:21:08 +0300

    add generics demo

---
+++ java-core/src/main/java/com/app/core/LearnApp.java
@@ -0,0 +1,12 @@
+package com.app.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LearnApp {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Arif");
+
+    }
+}
---
+++ java-core/src/main/java/com/app/interfacee/GreetingMessage.java
@@ -0,0 +1,6 @@
+package com.app.interfacee;
+
+@FunctionalInterface
+public interface GreetingMessage {
+    public abstract void greet(String name);
+}
